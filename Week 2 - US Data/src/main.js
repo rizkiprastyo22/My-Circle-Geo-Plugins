fccb31b1981de7642bd3ee92aa01ded40f6b60b1
Rizki Prastyo <rizkiprastyo22@gmail.com>
2021-06-13 17:27:50 +0700

    plugins

---
+++ Week 2 - US Data/src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/main.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import {
+  Icon
+} from "leaflet";
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+Vue.config.productionTip = false
+
+import 'leaflet/dist/leaflet.css'
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
